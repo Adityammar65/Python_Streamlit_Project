a33ad63a53476b0d5105ede52589afb7f100dcb3
Aditya Ammar <adityaammar65@gmail.com>
2025-07-10 08:23:28 +0700

    Update index.py
    Added toggle element

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
+info=st.toggle("You can toggle this")
+
 st.markdown(
     """
     # This is a testing code using streamlit framework
     Greetings everyone!
     """
 )
+
+if info:
+    st.markdown(
+        """
+        # My name is Ammar
+        I am a student in University of Amikom Yogyakarta
+        """
+    )
