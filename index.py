c1583e69dca1f603da33787cee62b3437dbf39f6
Aditya Ammar <adityaammar65@gmail.com>
2025-07-10 20:29:55 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-info=st.toggle("You can toggle this")
+st.title('Mean,Median,Mode calculator')
 
-st.markdown(
-    """
-    # This is a testing code using streamlit framework
-    Greetings everyone!
-    """
-)
+upload_file=st.file_uploader("Upload your file here", type=["xlsx","csv"])
+if upload_file is not None:
+    if upload_file.name.endswith('.xlsx'):
+        df=pd.read_excel(upload_file)
+    else:
+        df=pd.read_csv(upload_file)
 
-if info:
-    st.markdown(
-        """
-        # My name is Ammar
-        I am a student in University of Amikom Yogyakarta
-        """
-    )
+    st.write("Uploaded data:")
+    st.dataframe(df)
+
+    numeric_cols=df.select_dtypes(include='number').columns.tolist()
+    selected_col=st.selectbox("Choose column",numeric_cols)
+
+    if selected_col:
+        st.subheader(f"Result: {selected_col}")
+        st.write("**Mean:**",df[selected_col].mean())
+        st.write("**Median:**",df[selected_col].median())
+        st.write("**Mode:**",df[selected_col].mode().iloc[0])
